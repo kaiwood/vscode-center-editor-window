9f46376ee03a1479916ffc11d634496a879c24fd
Kai Wood <kwood@kwd.io>
2016-09-14 16:57:36 +0200

    Remove log message and do some cleanup

--- src/extension.ts
+++ src/extension.ts
@@ -6,11 +6,6 @@ import * as vscode from 'vscode';
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-
-    // Use the console to output diagnostic information (console.log) and errors (console.error)
-    // This line of code will only be executed once when your extension is activated
-    console.log('Congratulations, your extension "center-editor-window" is now active!');
-
     // The command has been defined in the package.json file
     // Now provide the implementation of the command with  registerCommand
     // The commandId parameter must match the command field in package.json
@@ -23,7 +18,6 @@ export function activate(context: vscode.ExtensionContext) {
         const selection = editor.selection;
         const range = new vscode.Range(selection.start, selection.end);
         editor.revealRange(range, vscode.TextEditorRevealType.InCenter);
-
     });
 
     context.subscriptions.push(disposable);
