a0a1851903148664c1040f9d167c80de67351ddc
Kai Wood <kwood@kwd.io>
2016-10-15 09:23:15 +0200

    Rename namespace of command
    From what I understand, best practice is to use the extensions name as namespace.

--- src/extension.ts
+++ src/extension.ts
@@ -9,7 +9,7 @@ export function activate(context: vscode.ExtensionContext) {
     // The command has been defined in the package.json file
     // Now provide the implementation of the command with  registerCommand
     // The commandId parameter must match the command field in package.json
-    let disposable = vscode.commands.registerCommand('extension.centerEditorWindow', () => {
+    let disposable = vscode.commands.registerCommand('center-editor-window.center', () => {
         // The code you place here will be executed every time your command is executed
         const editor = vscode.window.activeTextEditor;
         const selection = editor.selection;
