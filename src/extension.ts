4f2ecc4e52ac305a780ce3425a73afae5ad25de9
Kai Wood <kwood@kwd.io>
2017-12-27 08:10:39 +0100

    Make file Prettier

--- src/extension.ts
+++ src/extension.ts
@@ -1,25 +1,27 @@
-'use strict';
+"use strict";
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
+import * as vscode from "vscode";
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-    // The command has been defined in the package.json file
-    // Now provide the implementation of the command with  registerCommand
-    // The commandId parameter must match the command field in package.json
-    let disposable = vscode.commands.registerCommand('center-editor-window.center', () => {
-        // The code you place here will be executed every time your command is executed
-        const editor = vscode.window.activeTextEditor;
-        const selection = editor.selection;
-        const range = new vscode.Range(selection.start, selection.end);
-        editor.revealRange(range, vscode.TextEditorRevealType.InCenter);
-    });
+  // The command has been defined in the package.json file
+  // Now provide the implementation of the command with  registerCommand
+  // The commandId parameter must match the command field in package.json
+  let disposable = vscode.commands.registerCommand(
+    "center-editor-window.center",
+    () => {
+      // The code you place here will be executed every time your command is executed
+      const editor = vscode.window.activeTextEditor;
+      const selection = editor.selection;
+      const range = new vscode.Range(selection.start, selection.end);
+      editor.revealRange(range, vscode.TextEditorRevealType.InCenter);
+    }
+  );
 
-    context.subscriptions.push(disposable);
+  context.subscriptions.push(disposable);
 }
 
 // this method is called when your extension is deactivated
-export function deactivate() {
-}
+export function deactivate() {}
